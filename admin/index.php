2b067ba2139f891f37cd8087162e99eb5e6b2b58
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-20 14:47:17 +0700

    Membuat file index untuk dashboard admin

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,3 @@
-<?php 
-
-require '../function/function.php';
-
-if(isset($_POST[]))
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,36 +5,11 @@ if(isset($_POST[]))
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin</title>
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-	<link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-sm-6">
-				<div class="container border rounded shadow my-5 bg-white">
-					<h2 class="text-center my-3">Login</h2>
-					<hr>
-					<form action="" method="POST">
-						<div class="mb-3">
-							<div class="input-group">
-								<div class="input-group-text"><i class="bi bi-person-fill"></i></div>
-								<input type="text" name="username" id="username" class="form-control" autocomplete="off" placeholder="Masukan Username">
-							</div>
-						</div>
-						<div class="mb-3">
-							<div class="input-group">
-								<div class="input-group-text"><i class="bi bi-shield-lock-fill"></i></div>
-								<input type="text" name="password" id="password" class="form-control" autocomplete="off" placeholder="Masukan password">
-							</div>
-						</div>
-						<div class="mb-3">
-							<button type="submit" name="login" class="btn btn-primary col-12">Masuk</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+	
+	<h1>Hello</h1>
+
+	<script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
